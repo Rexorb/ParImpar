5b9f9b4b109c0876ef3308ba9705c1d331f0d8d0
Roberto Henrique <roberto_sonicteam@hotmail.com>
2025-05-19 08:42:35 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Olá, entre com um número");
+int num = Convert.ToInt16(Console.ReadLine());
+int resto = num % 2;
+if (resto == 0)
+{
+    Console.WriteLine("O número digitado é par");
+}
+else
+{
+    Console.WriteLine("O número digitado é ímpar");
+}
